ecce7b7f2d834ca5066f52237e5be57871589d88
Benjamin Hammer <hammerbenjamin@gmail.com>
2014-12-17 03:12:07 +0000

    FPS Mobile controller is working well

--- Assets/Resources/Scripts/TouchControllerNew.cs
+++ Assets/Resources/Scripts/TouchControllerNew.cs
@@ -39,9 +39,39 @@ public class TouchControllerNew : MonoBehaviour
 		private Vector3 velocity;						// Used for continuing momentum while in air
 		private bool canJump = true;
 
+		// Apply movement from move joystick
+		Vector3 ApplyMovement (Vector3 movement, Vector2 touchPadPosition)
+		{
+				var absJoyPos = new Vector2 (Mathf.Abs (touchPadPosition.x), Mathf.Abs (touchPadPosition.y));	
+				if (absJoyPos.y > absJoyPos.x) {
+						if (touchPadPosition.y > 0) //(touchPad.position.y > 0)
+								movement *= forwardSpeed * absJoyPos.y;
+						else
+								movement *= backwardSpeed * absJoyPos.y;
+				} else {
+						movement *= sidestepSpeed * absJoyPos.x;		
+				}
+				return movement;
+		}
+
+		void Build ()
+		{
+				gameObject.SendMessage ("OnBuildCube", SendMessageOptions.DontRequireReceiver);
+				Debug.Log ("build cube");
+		}
+	
+		void Jump ()
+		{
+				gameObject.SendMessage ("OnJump", SendMessageOptions.DontRequireReceiver);
+				Debug.Log ("jump");
+//				jump = true;
+//				canJump = true;
+		}
+	
 		// Use this for initialization
 		void Start ()
 		{
+				lastTime = Time.time;
 				// Cache component lookup at startup instead of doing this every frame		
 				thisTransform = GetComponent<Transform> ();
 				character = GetComponent<CharacterController> ();	
@@ -52,23 +82,109 @@ public class TouchControllerNew : MonoBehaviour
 						thisTransform.position = spawn.transform.position;
 				}
 		}
+
+		void RotatePlayer (Vector3 camRotation)
+		{
+				// Apply rotation from rotation joystick
+				//if (character.isGrounded) 
+				{
+//			var camRotation = val.Position;
+			
+						//						if (!touchPad.IsFingerOnLeftSide) {
+						//								if (touchPad.Touches.Count == 1) {
+						//										camRotation = touchPad.Touches [0].Position;// touchPad.position;
+						//								}
+						//						} else {
+						//								//								// Use tilt instead
+						////								//			print( iPhoneInput.acceleration );
+						////								var acceleration = Input.acceleration;
+						////								var absTiltX = Mathf.Abs (acceleration.x);
+						////								if (acceleration.z < 0 && acceleration.x < 0) {
+						////										if (absTiltX >= tiltPositiveYAxis)
+						////												camRotation.y = (absTiltX - tiltPositiveYAxis) / (1 - tiltPositiveYAxis);
+						////										else if (absTiltX <= tiltNegativeYAxis)
+						////												camRotation.y = -(tiltNegativeYAxis - absTiltX) / tiltNegativeYAxis;
+						////								}
+						////				
+						////								if (Mathf.Abs (acceleration.y) >= tiltXAxisMinimum)
+						////										camRotation.x = -(acceleration.y - tiltXAxisMinimum) / (1 - tiltXAxisMinimum);
+						//						}
+			
+						//										Debug.Log (camRotation);
+						camRotation.x *= rotationSpeed.x;
+						camRotation.y *= rotationSpeed.y;
+						camRotation *= Time.deltaTime;
+			
+						// Rotate the character around world-y using x-axis of joystick
+						thisTransform.Rotate (0, camRotation.x, 0, Space.World);
+			
+						// Rotate only the camera with y-axis input
+						cameraPivot.Rotate (-camRotation.y, 0, 0);
+				}
+		}
+	
+		bool jump = false;
 	
 		// Update is called once per frame
 		void Update ()
 		{
-//				Debug.Log (moveTouchPad.position);
-//				Debug.Log (touchPad.position.x + ", is left: " + touchPad.IsFingerOnLeftSide);
-
+		
+				var movement = Vector3.zero;
+		
+		
 				foreach (var touchInfo in touchPad.Touches) {
 						var val = touchInfo.Value;
+			
 						if (val.Touch.fingerId == 0 && val.IsOnLeftSide) {
 								Debug.Log ("Left finger on left side");
+				
+								movement = thisTransform.TransformDirection (new Vector3 (val.Position.x, 0, val.Position.y));
+								// We only want horizontal movement
+								movement.y = 0;
+								movement.Normalize ();
+								movement = ApplyMovement (movement, val.Position);
+//								// Check for jump
+//								if (character.isGrounded) {		
+//					
+//										var jump = false;
+//					
+//										//						if (!moveTouchPad.IsFingerDown ()) {
+//										//								canJump = true;
+//										//						}
+//					
+//										if (jump) {
+//												// Apply the current movement to launch velocity		
+//												velocity = character.velocity;
+//												velocity.y = jumpSpeed;
+//						
+//										}
+//								} else {			
+//										// Apply gravity to our velocity to diminish it over time
+//										velocity.y += Physics.gravity.y * Time.deltaTime;
+//					
+//										// Adjust additional movement while in-air
+//										movement.x *= inAirMultiplier;
+//										movement.z *= inAirMultiplier;
+//								}
+				
 								if (val.Touch.tapCount == 2) {
+										if (Helper.IsTimeUp (150f))
+												Build ();
 										Debug.Log ("Double Tap on with left finger on left side");
 								}
 						} else if (val.Touch.fingerId == 0 && !val.IsOnLeftSide) {
 								Debug.Log ("Left finger on right side");
+								RotatePlayer (val.Position);
 								if (val.Touch.tapCount == 2) {
+//										if (Helper.IsTimeUp (250f))
+										if (canJump && character.isGrounded) {
+												// Apply the current movement to launch velocity		
+												velocity = character.velocity;
+												velocity.y = jumpSpeed;
+												Jump ();
+												canJump = false;
+												lastTime = Time.time;
+										}
 										Debug.Log ("Double Tap on with left finger on right side");
 								}
 						}
@@ -79,79 +195,15 @@ public class TouchControllerNew : MonoBehaviour
 								}
 						} else if (val.Touch.fingerId == 1 && !val.IsOnLeftSide) {
 								Debug.Log ("Right finger on right side");
+								RotatePlayer (val.Position);
 								if (val.Touch.tapCount == 2) {
+										Jump ();
 										Debug.Log ("Double Tap on with right finger on right side");
 								}
 						}
 				}
-		
-				var touchPadPosition = Vector2.zero;
-				if (touchPad.Touches.Count == 1) {
-						touchPadPosition = touchPad.Touches [0].Position;
-//						Debug.Log (touchPad.Touches [0].Id);
-				}
 
-				if (touchPad.Touches.Count == 2) {
-						var left = touchPad.Touches [0];
-						var right = touchPad.Touches [1];
-//						Debug.Log (left.Id + ": " + left.Position);
-//						Debug.Log (right.Id + ": " + right.Position);
-				}
-		
-		
-				//				var movement = thisTransform.TransformDirection (new Vector3 (touchPad.position.x, 0, touchPad.position.y));
-				var movement = thisTransform.TransformDirection (new Vector3 (touchPadPosition.x, 0, touchPadPosition.y));
-		
-				// We only want horizontal movement
-				movement.y = 0;
-				movement.Normalize ();
-				if (touchPad.IsFingerOnLeftSide) {
-
-						
-						// Apply movement from move joystick
-//			var absJoyPos = new Vector2 (Mathf.Abs (touchPad.position.x), Mathf.Abs (touchPad.position.y));	
-						var absJoyPos = new Vector2 (Mathf.Abs (touchPadPosition.x), Mathf.Abs (touchPadPosition.y));	
-						if (absJoyPos.y > absJoyPos.x) {
-
-								if (touchPadPosition.y > 0) //(touchPad.position.y > 0)
-										movement *= forwardSpeed * absJoyPos.y;
-								else
-										movement *= backwardSpeed * absJoyPos.y;
-						} else {
-								movement *= sidestepSpeed * absJoyPos.x;		
-						}
-				}
-
-				// Check for jump
-				if (character.isGrounded) {		
-			
-						var jump = false;
-			
-//						if (!moveTouchPad.IsFingerDown ()) {
-//								canJump = true;
-//						}
-			
-						if (touchPad.tapCount >= 2 && touchPad.IsFingerOnLeftSide) {
-								gameObject.SendMessage ("OnBuildCube", SendMessageOptions.DontRequireReceiver);
-								Debug.Log ("build cube");
-						}
-						if (touchPad.tapCount >= 2 && !touchPad.IsFingerOnLeftSide) {
-//								if (canJump) {
-								gameObject.SendMessage ("OnJump", SendMessageOptions.DontRequireReceiver);
-								Debug.Log ("jump");
-								jump = true;
-								canJump = false;
-//								}
-						}
-			
-						if (jump) {
-								// Apply the current movement to launch velocity		
-								velocity = character.velocity;
-								velocity.y = jumpSpeed;
-//								GameObject.FindObjectOfType<Player> ().Jump ();
-				
-						}
-				} else {			
+				if (!character.isGrounded) {
 						// Apply gravity to our velocity to diminish it over time
 						velocity.y += Physics.gravity.y * Time.deltaTime;
 			
@@ -166,57 +218,30 @@ public class TouchControllerNew : MonoBehaviour
 		
 				// Actually move the character	
 				character.Move (movement);
+
+		
 		
 				if (character.isGrounded) {
 						// Remove any persistent velocity after landing	
 						velocity = Vector3.zero;
 				}
-		
-				// Apply rotation from rotation joystick
-//				if (character.isGrounded) 
-				{
-						var camRotation = Vector2.zero;
 
-//						Debug.Log (rotateTouchPad.position);
-
-						if (touchPad.Touches.Count == 1 && !touchPad.Touches [0].IsOnLeftSide) {
-								camRotation = touchPad.Touches [0].Position;
-						} else if (touchPad.Touches.Count == 2 && !touchPad.Touches [1].IsOnLeftSide) {
-								camRotation = touchPad.Touches [1].Position;
-						}
-
-//						if (!touchPad.IsFingerOnLeftSide) {
-//								if (touchPad.Touches.Count == 1) {
-//										camRotation = touchPad.Touches [0].Position;// touchPad.position;
-//								}
-//						} else {
-//								//								// Use tilt instead
-////								//			print( iPhoneInput.acceleration );
-////								var acceleration = Input.acceleration;
-////								var absTiltX = Mathf.Abs (acceleration.x);
-////								if (acceleration.z < 0 && acceleration.x < 0) {
-////										if (absTiltX >= tiltPositiveYAxis)
-////												camRotation.y = (absTiltX - tiltPositiveYAxis) / (1 - tiltPositiveYAxis);
-////										else if (absTiltX <= tiltNegativeYAxis)
-////												camRotation.y = -(tiltNegativeYAxis - absTiltX) / tiltNegativeYAxis;
-////								}
-////				
-////								if (Mathf.Abs (acceleration.y) >= tiltXAxisMinimum)
-////										camRotation.x = -(acceleration.y - tiltXAxisMinimum) / (1 - tiltXAxisMinimum);
-//						}
-			
-						camRotation.x *= rotationSpeed.x;
-						camRotation.y *= rotationSpeed.y;
-						camRotation *= Time.deltaTime;
-			
-						// Rotate the character around world-y using x-axis of joystick
-						thisTransform.Rotate (0, camRotation.x, 0, Space.World);
-			
-						// Rotate only the camera with y-axis input
-						cameraPivot.Rotate (-camRotation.y, 0, 0);
+				if (IsTimeUp (250f)) {
+						canJump = true;
 				}
 		}
 
+		private float lastTime;
+	
+		public bool IsTimeUp (float timeLimit)
+		{
+				if ((Time.time - lastTime) * 1000f > timeLimit) {
+						lastTime = Time.time;
+						return true;
+				}
+				return false;
+		}
+	
 		void OnEndGame ()
 		{
 				touchPad.Disable ();
